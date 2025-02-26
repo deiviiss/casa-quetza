839557bb304af97007ece44fd181cf4408d829c5
David Hilera <david.hilera@hotmail.com>
2025-02-26 14:26:24 -0600

    feat: enhance TopMenu with icons and improved layout

--- src/components/TopMenu.tsx
+++ src/components/TopMenu.tsx
@@ -4,6 +4,8 @@ import Image from 'next/image'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 import { useEffect, useState } from 'react'
+import { FaShopLock } from 'react-icons/fa6'
+import { IoHomeOutline, IoPeopleOutline } from 'react-icons/io5'
 
 export const TopMenu = () => {
   const logo = '/imgs/logoWeb.png'
@@ -29,7 +31,7 @@ export const TopMenu = () => {
   }, [])
 
   return (
-    <div className={`w-full fixed top-0 z-20 text-white ${isProductDetail ? 'bg-slate-800' : bgColor} transition-colors duration-300`}>
+    <div className={`w-full fixed top-0 z-20 text-white ${isProductDetail ? 'bg-slate-800' : bgColor} transition-colors duration-300 py-1`}>
       <header className="container mx-auto lg:px-20 px-4 py-1 flex justify-between items-center">
         <Link href={'/'}>
           <div className="flex items-center space-x-2">
@@ -38,9 +40,30 @@ export const TopMenu = () => {
         </Link>
         <nav>
           <ul className="space-x-4 flex">
-            <li><Link href="/" className="hover:text-slate-200 transition-transform">Inicio</Link></li>
-            <li><Link href="/memberships" className="hover:text-slate-200 transition-transform">Membresías</Link></li>
-            <li><Link href="/products" className="hover:text-slate-200 transition-transform">Dispensario digital</Link></li>
+            <li>
+              <Link href="/" className="hover:text-slate-200 transition-transform flex items-center space-x-2">
+                <IoHomeOutline size={18} />
+                <span className='hidden sm:inline'>
+                  Inicio
+                </span>
+              </Link>
+            </li>
+            <li>
+              <Link href="/memberships" className="hover:text-slate-200 transition-transform flex items-center space-x-2">
+                <IoPeopleOutline size={20} />
+                <span className='hidden sm:inline'>
+                  Membresías
+                </span>
+              </Link>
+            </li>
+            <li>
+              <Link href="/products" className="hover:text-slate-200 transition-transform flex items-center space-x-2">
+                <FaShopLock size={17} />
+                <span className='hidden sm:inline'>
+                  Dispensario digital
+                </span>
+              </Link>
+            </li>
           </ul>
         </nav>
       </header>
